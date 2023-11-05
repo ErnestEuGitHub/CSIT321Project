7f63bc09ebd4d680992b4c1e4ac3255b0cbe8863
Ernest Eu <116449040+ErnestEuGitHub@users.noreply.github.com>
2023-11-06 00:21:53 +0800

    added connection to database as testing

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
+from database import engine
+from sqlalchemy import text
 
 app = Flask(__name__)
 
+def load_desc_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("Select * from testtable"))
+        rows = result.fetchall()
+
+        descriptions = [row._asdict() for row in rows]
+    return descriptions
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    desc = load_desc_from_db()
+    return render_template('index.html', desc=desc)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
