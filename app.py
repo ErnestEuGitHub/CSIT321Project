6d2be7d94aaad32942f896d62d1b986dc5c90d3e
Ernest Eu <sim.ernesteu@gmail.com>
2023-11-11 09:20:24 +0800

    Login commits, codes still not working yet.

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from database import engine
 from sqlalchemy import text
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 
 def load_desc_from_db():
     with engine.connect() as conn:
@@ -19,14 +20,34 @@ def loadhome():
     
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    invalidEmail = False
+    invalidPassword = False
+    emptyForm = False
+    error = False
+
     if request.method == "POST":
         email = request.form["email"]
         password = request.form["password"]
 
+        if not email or not password:
+            return render_template('login.html', emptyForm = True)
+
         with engine.connect() as conn:
-            searchUser = conn.execute(text("SELECT * FROM USERS WHERE EMAIL = " + email))
+            searchUser = conn.execute(text("SELECT * FROM users WHERE EMAIL = '" + email +"'"))
             rows = searchUser.fetchall()
-        return rows
+            print(rows)
+
+            if not rows:
+                return render_template('login.html', invalidEmail = True)
+            elif rows[3] == password:
+                flash('Login Successful!')
+                return render_template('login.html', invalidEmail = False, invalidPassword = False)
+            elif rows[3] != password:
+                flash('Help lah, you forgot your password!')
+                return render_template('login.html', invalidPassword = True,)
+            else:
+                flash('Oops, an error has occured.')
+
     else:
         return render_template('login.html')
 
