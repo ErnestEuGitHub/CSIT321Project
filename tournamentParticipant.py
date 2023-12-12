e500d1ec81299882152d8c625d899e71452c4e42
VannessChin <vannesschin1398@gmail.com>
2023-12-12 18:40:48 +0800

    connect database

--- tournamentParticipant.py
+++ tournamentParticipant.py
@@ -4,4 +4,12 @@ import bcrypt
 from sqlalchemy import text
 
 def tournamentParticipant():
+    
+    with dbConnect.engine.connect() as conn:
+            query = "SELECT * FROM participants"
+            result = conn.execute(text(query))
+            rows = result.fetchall()
+
+            participantsOptions = [row._asdict() for row in rows]
+    
     return render_template('tournamentParticipant.html')
