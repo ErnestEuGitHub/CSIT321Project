3d741c00ac347a548d0bf305ed79b9f5728f5e68
Ernest Eu <sim.ernesteu@gmail.com>
2023-12-17 11:45:57 +0800

    added 404 not found page, error handling and tour owner checks

--- app.py
+++ app.py
@@ -81,10 +81,25 @@ def loaddashboard():
 
 @app.route('/settings/<tourID>', methods=["POST", "GET"])
 def loadsettings(tourID):
-    page = settings(tourID)
-    return page
+    if "id" not in session:
+        return redirect(url_for('loadLogin'))
+    else:
+        with dbConnect.engine.connect() as conn:
+            query = "SELECT * from tournaments WHERE userID = :userID AND tourID = :tourID"
+            inputs = {'userID': session["id"], 'tourID': tourID}
+            checktour = conn.execute(text(query), inputs)
+            rows = checktour.fetchall()
 
+            if rows:
+                page = settings(tourID)
+                return page
+            
+            else:
+                return render_template('notfound.html')
 
+@app.errorhandler(404)
+def loadnotfound(error):
+    return render_template('notfound.html', error=error)
 
 if __name__ == "__main__":
     
