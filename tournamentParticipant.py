606c69b0e4efe7523b9cce9048d8a3b8d0516e8a
VannessChin <vannesschin1398@gmail.com>
2023-12-21 03:17:40 +0800

    listing out participants list

--- tournamentParticipant.py
+++ tournamentParticipant.py
@@ -4,12 +4,37 @@ import bcrypt
 from sqlalchemy import text
 
 def tournamentParticipant():
-    
-    with dbConnect.engine.connect() as conn:
-            query = "SELECT * FROM participants"
-            result = conn.execute(text(query))
-            rows = result.fetchall()
+        try:
+                with dbConnect.engine.connect() as conn:
+                
+                        # Query the 'participant' table
+                        query = "SELECT * FROM participants"
+                        getparticipants = conn.execute(text(query))
+                        participants = getparticipants.fetchall()
 
-            participantsOptions = [row._asdict() for row in rows]
+                        # Convert the result to a list of dictionaries
+                        participantsList = [participant._asdict() for participant in participants]
+                        
+                        # Render the HTML template with the participant data
+                        return participantsList
+        
+        
+
+        except Exception as e:
+                # Handle exceptions (e.g., database connection error)
+                print(f"Error: {e}")
+                flash("An error occurred while retrieving participant data.", "error")
+                return render_template('tournamentDashboard.html')  # Create an 'error.html' template for error handling
+
+def updateNavProjects():
+        with dbConnect.engine.connect() as conn:
+            query = "SELECT * FROM projects WHERE userID = :userID;"
+            inputs = {'userID': session["id"]}
+            getprojs = conn.execute(text(query), inputs)
+            rows = getprojs.fetchall()
+
+            projects = [row._asdict() for row in rows]
+
+            session["projnav"] = projects
+            return projects       
     
-    return render_template('tournamentParticipant.html')
