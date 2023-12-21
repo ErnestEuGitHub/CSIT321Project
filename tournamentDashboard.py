37d08768bf27366caa98fe5f70f72d13afcff0c5
wwwfreedomwww <jesperfanyz@gmail.com>
2023-12-22 00:23:27 +0800

    Revert "Merge pull request #26 from ErnestEuGitHub/participants"
    This reverts commit c8488a3e5dc4e92cad262e48500bb69bdb87fa45, reversing
    changes made to f32caf611fe311e9f739d9232aaa174f86c2b1d4.

--- tournamentDashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import render_template, request, flash, session
-from database import dbConnect
-import bcrypt
-from sqlalchemy import text
-
-def tournamentDashboard():
-    return render_template('tournamentDashboard.html')
