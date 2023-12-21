48e5369d40c4fa710ac4eb0721d56d49295a6f7e
wwwfreedomwww <jesperfanyz@gmail.com>
2023-12-21 23:20:20 +0800

    update createstructure

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from general import *
 from user import *
 from tournaments import *
 from projects import *
+from createStructure import *
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
