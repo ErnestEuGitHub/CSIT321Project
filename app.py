850a4d46ce533d82f301b4ae12b539481fbf6aac
VannessChin <vannesschin1398@gmail.com>
2023-12-21 22:43:20 +0800

    remove tournamentDashboard due to merging conflict

--- app.py
+++ app.py
@@ -150,10 +150,7 @@ def loadcreateStructure():
     # if "id" not in session:
     #     return redirect(url_for('loadLogin'))
     page = createStructure()
-@app.route('/tournamentDashboard', methods=["POST", "GET"])
-def loadTournamentDashboard():
-    page = tournamentDashboard()
-    return page
+
 
 @app.route('/tournamentParticipant', methods=["POST", "GET"])
 def loadTournamentParticipant():
