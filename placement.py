e81803dc0aac10740c6c7154c260abfa8eb14aaf
Ernest Eu <sim.ernesteu@gmail.com>
2023-12-23 01:10:48 +0800

    Linked Placement and Structure

--- placement.py
+++
@@ -1,5 +0,0 @@
-def get_updated_content():
-    # Logic to read and return updated content from seeding.html
-    with open('templates\seeding.html', 'r') as file:   
-        updated_content = file.read()
-    return updated_content
