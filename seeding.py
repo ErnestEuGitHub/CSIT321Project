f9ce751dd66eca9fa3e3c799a7407dd6c93a00f2
Ernest Eu <sim.ernesteu@gmail.com>
2024-01-08 14:52:14 +0800

    Added Brackets for singleElim Page

--- seeding.py
+++ seeding.py
@@ -8,14 +8,16 @@ def seeding(projID, tourID, stageID):
     navtype = 'dashboard'
     tournamentName = retrieveDashboardNavName(tourID)
 
+    #stageformat 1 is Single Elim, 2 is Double Elim, 3 is Single RR, 4 is Double RR
     with dbConnect.engine.connect() as conn:
-        query = "SELECT numberOfParticipants, numberOfGroups FROM stages WHERE stageID = :stageID"
+        query = "SELECT numberOfParticipants, numberOfGroups, stageFormatID FROM stages WHERE stageID = :stageID"
         inputs = {'stageID': stageID}
         result = conn.execute(text(query), inputs)
         rows = result.fetchall()
 
         numberOfParticipants = rows[0][0]
         numberOfGroups = rows[0][1]
+        stageForm = rows[0][2]
 
         query = "SELECT participantID, participantName FROM participants WHERE tourID = :tourID"
         inputs = {'tourID': tourID}
@@ -25,6 +27,7 @@ def seeding(projID, tourID, stageID):
         participantID = [row._asdict() for row in rows]
         participantName = [row._asdict() for row in rows]
 
-        count = 1
-
-    return render_template('seeding.html', participantID=participantID, participantName=participantName, numberOfParticipants=numberOfParticipants, numberOfGroups=numberOfGroups, navtype=navtype, tournamentName=tournamentName, tourID=tourID, projID=projID, count=count)
+        if stageForm == 1 or stageForm == 2:
+            return render_template('seedingSingleElim.html', participantID=participantID, participantName=participantName, numberOfParticipants=numberOfParticipants, numberOfGroups=numberOfGroups, navtype=navtype, tournamentName=tournamentName, tourID=tourID, projID=projID)
+        else:
+            return render_template('seeding.html', participantID=participantID, participantName=participantName, numberOfParticipants=numberOfParticipants, numberOfGroups=numberOfGroups, navtype=navtype, tournamentName=tournamentName, tourID=tourID, projID=projID)
