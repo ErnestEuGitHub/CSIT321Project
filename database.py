21d212d7ae2e1f2563fd261e15464279e5b6eb84
Ernest Eu <sim.ernesteu@gmail.com>
2024-04-14 19:55:57 +0800

    updated db

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 class dbConnect:
-    db_connection_string = "mysql+pymysql://ymxyk9vwlplokqz9epql:pscale_pw_yTBmlW78dzZDPiag3SXdEtAIXlf3r1j3c1dHlLsFjg2@aws.connect.psdb.cloud/csit321?charset=utf8mb4"
+    db_connection_string = "mysql+pymysql://9ifgazidbdynts8z5mhn:pscale_pw_M2Z17oTDOSuKopVqTLhLvZc1JPVeFnDkWRdFspL83eb@aws.connect.psdb.cloud/csit321?charset=utf8mb4"
 
     engine = create_engine(
         db_connection_string,
