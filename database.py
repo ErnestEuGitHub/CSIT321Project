0dffde63026609c286029ce662a9e0466747af80
Ernest Eu <sim.ernesteu@gmail.com>
2024-04-14 19:48:38 +0800

    Revert "Updated db"
    This reverts commit 91b499979528b43fc706c8bec544bd4661e42695.

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine, text
 
 class dbConnect:
-    db_connection_string = "mysql+pymysql://oslz0wcxffrpqea60rna:pscale_pw_q95yltu3ILbRH8ueq9TY8sZRtHsJd9cxLc40Wr8hD4n@aws.connect.psdb.cloud/csit321?charset=utf8mb4"
+    db_connection_string = "mysql+pymysql://ymxyk9vwlplokqz9epql:pscale_pw_yTBmlW78dzZDPiag3SXdEtAIXlf3r1j3c1dHlLsFjg2@aws.connect.psdb.cloud/csit321?charset=utf8mb4"
 
     engine = create_engine(
         db_connection_string,
